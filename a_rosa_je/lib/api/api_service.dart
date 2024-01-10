6d588e088459e0f9228eb9e45bce672b3071d79e
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-01-10 12:54:45 +0100

    add api

--- a_rosa_je/lib/api/api_service.dart
+++ a_rosa_je/lib/api/api_service.dart
@@ -2,9 +2,11 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../util/annonce.dart';
 
+//ip ordi fixe : 192.168.56.1
+//ip ordi portable : 192.168.71.1
 class ApiService {
   Future<List<Annonce>> fetchAnnonces() async {
-    final response = await http.get(Uri.parse('http://192.168.56.1:1212/annonce'));
+    final response = await http.get(Uri.parse('http://192.168.71.1:1212/annonce'));
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
