1d72f0fb893b931209e438345b6db0cbce036ac9
alexandreboz <bozzomvp@gmail.com>
2024-02-07 19:51:50 +0100

    correction push

--- a_rosa_je/lib/api/api_service.dart
+++ a_rosa_je/lib/api/api_service.dart
@@ -1,22 +1,3 @@
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import '../util/annonce.dart';
-
-// //ip ordi fixe : 192.168.56.1
-// //ip ordi portable : 192.168.71.1
-// //localhost: http://localhost:1212/home/city/Montpellier
-// class ApiService {
-//   Future<List<Annonce>> fetchAnnonces(String city) async {
-//     final response = await http.get(Uri.parse('http://localhost:1212/home/city/$city'));
-
-//     if (response.statusCode == 200) {
-//       List jsonResponse = json.decode(response.body);
-//       return jsonResponse.map((data) => Annonce.fromJson(data)).toList();
-//     } else {
-//       throw Exception('Failed to load annonces from API');
-//     }
-//   }
-// }
 import 'package:a_rosa_je/util/annonce.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -58,9 +39,12 @@ class ApiService {
       return {'success': false, 'error': errorMessage};
     }
   }
+}
 
-  Future<List<Annonce>> fetchAnnonces(String city) async {
-    final response = await http.get(Uri.parse('http://localhost:1212/home/city/$city'));
+class ApiAnnoncesVille {
+  Future<List<Annonce>> fetchAnnoncesVille(String city) async {
+    final response =
+        await http.get(Uri.parse('http://localhost:1212/home/city/$city'));
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
@@ -71,10 +55,10 @@ class ApiService {
   }
 }
 
-//api pour récupérer les annonces par User
 class ApiAnnoncesUser {
   Future<List<Annonce>> fetchAnnoncesUser(String idUser) async {
-    final response = await http.get(Uri.parse('http://localhost:1212/profile/profileAds/$idUser'));
+    final response = await http
+        .get(Uri.parse('http://localhost:1212/profile/profileAds/$idUser'));
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body);
@@ -85,7 +69,6 @@ class ApiAnnoncesUser {
   }
 }
 
-//api pour créer une annonce
 class ApiCreateAnnounce {
   static Future<http.Response> createAnnounce({
     required String title,
@@ -97,17 +80,17 @@ class ApiCreateAnnounce {
     required String endDate,
   }) async {
     final url = Uri.parse('http://localhost:1212/create_adv');
-    final response = await http.post(url,
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode({
-          "title": title,
-          "created_at": createdAt,
-          "idPlant": idPlant,
-          "idUser": idUser,
-          "description": description,
-          "start_date": startDate,
-          "end_date": endDate,
-        }));
+    final response = await http.post(url, headers: {
+      "Content-Type": "application/json"
+    }, body: {
+      "title": title,
+      "created_at": createdAt,
+      "idPlant": idPlant,
+      "idUser": idUser,
+      "description": description,
+      "start_date": startDate,
+      "end_date": endDate,
+    });
 
     return response;
   }
