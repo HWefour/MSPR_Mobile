9981db0edff5112eb82271cae55d32bce4beb38b
alexandreboz <bozzomvp@gmail.com>
2024-02-08 19:51:32 +0100

    pull dev

--- a_rosa_je/.dart_tool/dartpad/web_plugin_registrant.dart
+++ a_rosa_je/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,16 +3,6 @@
 // Generated file. Do not edit.
 //
 
-// @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:image_picker_for_web/image_picker_for_web.dart';
-import 'package:location_web/location_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
-void registerPlugins([final Registrar? pluginRegistrar]) {
-  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  ImagePickerPlugin.registerWith(registrar);
-  LocationWebPlugin.registerWith(registrar);
-  registrar.registerMessageHandler();
-}
+void registerPlugins() {}
